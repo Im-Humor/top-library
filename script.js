d70b083f3f96fb1ba23dec30f65049a8391e6dda
Im-Humor <orangite@gmail.com>
2023-07-06 12:56:30 -0700

    removed card placeholders, added logic for adding  form fields via js

--- script.js
+++ script.js
@@ -7,38 +7,63 @@ function Book(author, title, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary(author, title, pages, read) {
-    const book1 = new Book(author, title, pages, read)
-    myLibrary.push(book1)
+const librarySide = document.querySelector(".library-side");
+const formSide = document.querySelector(".form-side")
+const showButton = document.querySelector("#book-button")
+
+function addBookToLibrary() {
+    for (let x = 0; x < myLibrary.length; x++) {
+        const newCard = document.createElement("div");
+        newCard.classList.add('card');
+    
+        const newList = document.createElement("ul");
+    
+        for (let y in myLibrary[x]) {
+            if (myLibrary[x][y] == 0) {
+                myLibrary[x][y] = "No"
+            }
+            else if (myLibrary[x][y] == 1) {
+                myLibrary[x][y] = "Yes"
+            }
+            const listItem = document.createElement("li");
+            const itemText = document.createTextNode(y + ": " + myLibrary[x][y]);
+            listItem.appendChild(itemText);
+            newList.appendChild(listItem);
+        }
+    
+        newCard.appendChild(newList);
+    
+        librarySide.appendChild(newCard);
+    }
 }
 
-librarySide = document.querySelector(".library-side");
+function showForm() {
+    showButton.style.display = "none"
+    const bookForm = document.createElement("form")
+    bookForm.classList.add("book-form")
 
-addBookToLibrary("JK Rowling", "Harry Potter", 300, 0);
-addBookToLibrary("Dune Man", "Dune", 1000, 0);
+    formFields = ["Author", "Title", "Page count", "Been read?"]
 
-
-for (let x = 0; x < myLibrary.length; x++) {
-    const newCard = document.createElement("div");
-    newCard.classList.add('card');
-
-    const newList = document.createElement("ul");
-
-    for (let y in myLibrary[x]) {
-        if (myLibrary[x][y] == 0) {
-            myLibrary[x][y] = "No"
-        }
-        else if (myLibrary[x][y] == 1) {
-            myLibrary[x][y] = "Yes"
-        }
-        const listItem = document.createElement("li");
-        const itemText = document.createTextNode(y + ": " + myLibrary[x][y]);
-        listItem.appendChild(itemText);
-        newList.appendChild(listItem);
+    for (let x = 0; x < formFields.length; x++) {
+        const formLabel = document.createElement("label");
+        formLabel.textContent = formFields[x];
+        formLabel.setAttribute("for", x);
+        bookForm.appendChild(formLabel);
+        const formInput = document.createElement("input");
+        formInput.setAttribute("type", "text");
+        formInput.setAttribute("name", formFields[x]);
+        formInput.setAttribute("id", formFields[x]);
+        bookForm.appendChild(formInput);
     }
 
-    newCard.appendChild(newList);
-
-    librarySide.appendChild(newCard);
+    formSide.appendChild(bookForm)
 }
 
+
+
+
+
+
+//I think I'll use this when submitting a new book?
+//const book1 = new Book(author, title, pages, read)
+//myLibrary.push(book1)
