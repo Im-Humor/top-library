e21d56fe8bcaa0471493dcce3f659cb35bae21ea
Im-Humor <orangite@gmail.com>
2023-07-05 22:15:01 -0700

    add styling for book info cards, add card creation to script

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
+function Book(author, title, pages, read) {
     this.author = author;
+    this.title = title;
     this.pages = pages;
     this.read = read;
-    this.info = () => {
-        return `${title}, ${pages} pages, ${read}`
-    }
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+function addBookToLibrary(author, title, pages, read) {
+    const book1 = new Book(author, title, pages, read)
+    myLibrary.push(book1)
 }
+
+librarySide = document.querySelector(".library-side");
+
+addBookToLibrary("JK Rowling", "Harry Potter", 300, 0);
+addBookToLibrary("Dune Man", "Dune", 1000, 0);
+
+
+for (let x = 0; x < myLibrary.length; x++) {
+    const newCard = document.createElement("div");
+    newCard.classList.add('card');
+
+    const newList = document.createElement("ul");
+
+    for (let y in myLibrary[x]) {
+        if (myLibrary[x][y] == 0) {
+            myLibrary[x][y] = "No"
+        }
+        else if (myLibrary[x][y] == 1) {
+            myLibrary[x][y] = "Yes"
+        }
+        const listItem = document.createElement("li");
+        const itemText = document.createTextNode(y + ": " + myLibrary[x][y]);
+        listItem.appendChild(itemText);
+        newList.appendChild(listItem);
+    }
+
+    newCard.appendChild(newList);
+
+    librarySide.appendChild(newCard);
+}
+
