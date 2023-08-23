226b1f5cee9bed801c19f3f01545129b32d370e1
Im-Humor <orangite@gmail.com>
2023-08-23 11:04:23 -0700

    create library page with class based book objects, still need to add delete button

--- script.js
+++ script.js
@@ -1,127 +1,64 @@
-let myLibrary = [];
-
-function Book(author, title, pages, read) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(author, title, pages, read_status) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.read_status = read_status;
+        this.book_id = Math.floor(Math.random() * 1000000)
+    }
 }
 
-//I made this a prototype function because the lesson said to
-//but I don't use 'this' or anything that allows it to be reusable
-// upon review i don't think prototypes use 'this', maybe
-// also, the reason i'm using slice to shorten the title
-// is because i call this function on the id of an html element
-// and the id's are created with a del-[title] or read-[title] 
-// subheader based on the action needed for the read/delete buttons
-Book.prototype.readBook = function(title) {
-    newTitle = title.slice(5)
-    const objectPos = myLibrary.findIndex(item => item.title == newTitle);
-    myLibrary[objectPos].read = Boolean(1 - myLibrary[objectPos].read);
-    updateCards();
-}
+//modularize card creation into BookCard class?
 
-const librarySide = document.querySelector(".library-side");
-const formSide = document.querySelector(".form-side")
-const showButton = document.querySelector("#book-button")
+libraryInventory = [];
 
-// adds new book object to library at end of array
-// and also calls updateCards
-function addBookToLibrary(author, title, pages, read) {
-    const book1 = new Book(author, title, pages, read);
-    myLibrary.push(book1);
-    updateCards();
-}
-
-function removeBook(title) {
-    newTitle = title.slice(4)
-    const objectPos = myLibrary.findIndex(item => item.title == newTitle);
-    myLibrary.splice(objectPos, 1);
-    updateCards();
-}
-
-
-// removes existing list of cards (if existant),
-// then iterates through myLibrary and adds all
-// book cards to the DOM
-function updateCards() {
-    librarySide.innerHTML="";
-    for (let x = 0; x < myLibrary.length; x++) {
-        const newCard = document.createElement("div");
-        newCard.classList.add('card');
-    
-        const newList = document.createElement("ul");
-    
-        const libFields = ['author', 'title', 'pages', 'read'];
-        for (let y = 0; y < libFields.length; y++) {
-            let iterField = myLibrary[x][libFields[y]];
-            const listItem = document.createElement("li");
-            const itemText = document.createTextNode(libFields[y] + ": " + iterField);
-            listItem.appendChild(itemText);
-            newList.appendChild(listItem);
+const library = (() => {
+    const libraryContainer = document.querySelector(".library-container")
+    const refreshLibrary = () => {
+        libraryContainer.innerHTML = "";
+        for (let x = 0; x < libraryInventory.length; x++) {
+            const newCard = document.createElement("div")
+            newCard.classList.add("book-card")
+            const newCardAuthor = document.createElement("div")
+            newCardAuthor.classList.add("book-author")
+            newCardAuthor.innerText = libraryInventory[x]["author"]
+            const newCardTitle = document.createElement("div")
+            newCardTitle.classList.add("book-author")
+            newCardTitle.innerText = libraryInventory[x]["title"]
+            const newCardPages = document.createElement("div")
+            newCardPages.classList.add("book-pages")
+            newCardPages.innerText = libraryInventory[x]["pages"]
+            const newCardReadStatus = document.createElement("div")
+            newCardReadStatus.classList.add("book-read-status")
+            newCardReadStatus.innerText = libraryInventory[x]["read_status"]
+            newCard.appendChild(newCardAuthor)
+            newCard.appendChild(newCardTitle)
+            newCard.appendChild(newCardPages)
+            newCard.appendChild(newCardReadStatus)
+            libraryContainer.appendChild(newCard)
         }
-    
-        newCard.appendChild(newList);
-
-        //add delete button to each object's card
-        //assign an id to the card of the title
-        //add listener to remove event's target by ID
-        const deleteButton = document.createElement("button");
-        deleteButton.setAttribute("type", "button");
-        deleteButton.innerText = "X";
-        deleteButton.setAttribute("id", `del-${myLibrary[x].title}`)
-        deleteButton.addEventListener("click", event => {
-            removeBook(event.target.id);
-        })
-        newCard.appendChild(deleteButton);
-
-        //lol the below works but adjust to use
-        //true / false and remove weird branching
-        const readButton = document.createElement("button");
-        readButton.setAttribute("type", "button");
-        readButton.innerText = "Read?";
-        readButton.setAttribute("id", `read-${myLibrary[x].title}`);
-        readButton.addEventListener("click", event => {
-            myLibrary[x].readBook(event.target.id);
-        })
-        newCard.appendChild(readButton);
-
-        librarySide.appendChild(newCard);
-    }
-}
-
-// on 'add book' button click, button disappears and
-// form is added to DOM to accept user input.
-// submit button's onclick attribute calls addBookToLibrary function
-// to add the form submission information to the form,
-// which also updates the visible book cards
-function showForm() {
-    showButton.style.display = "none";
-    const bookForm = document.createElement("form");
-
-    formFields = ["Author", "Title", "Page count", "Been read?"];
-
-    for (let x = 0; x < formFields.length; x++) {
-        const formLabel = document.createElement("label");
-        formLabel.textContent = formFields[x];
-        formLabel.setAttribute("for", x);
-        bookForm.appendChild(formLabel);
-        const formInput = document.createElement("input");
-        formInput.setAttribute("type", "text");
-        formInput.setAttribute("name", formFields[x]);
-        formInput.setAttribute("id", formFields[x]);
-        bookForm.appendChild(formInput);
     }
 
-    const submitButton = document.createElement("button");
-    submitButton.innerText = 'Submit';
-    submitButton.setAttribute("type", "button");
-    submitButton.setAttribute("onclick", `addBookToLibrary(document.getElementById("Author").value, document.getElementById("Title").value,
-    document.getElementById("Page count").value, (document.getElementById("Been read?").value === "true"))`);
-    
-    bookForm.appendChild(submitButton);
 
-    formSide.appendChild(bookForm);
 
-}
+    return {refreshLibrary}
+})();
 
+const bookForm = (() => {
+    const submitButton = document.getElementById("submit_button")
+    let authorField = document.getElementById("book_author")
+    let titleField = document.getElementById("book_title")
+    let pagesField = document.getElementById("book_pages")
+    let readStatusField = document.getElementById("book_read_status")
+    const addToLibrary = (event) => {
+        let newBook = new Book(authorField.value, titleField.value, 
+            pagesField.value, readStatusField.value)
+        libraryInventory.push(newBook);
+        library.refreshLibrary();
+    }
+    return {submitButton, addToLibrary}
+})();
+
+bookForm.submitButton.addEventListener("click", bookForm.addToLibrary)
+
+library.refreshLibrary()
